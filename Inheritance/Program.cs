9e88299ed9ff0d59aa5e78769fe16c80e116036d
andriantri09 <andrian.09@students.amikom.ac.id>
2018-10-16 00:26:13 +0700

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new person();
+            person.Name = "Asta";
+            person.Addres = "Kerajaan Clover";
+            person.Ngomong();
+            person.TempatId();
+
+            var student = new Student();
+            student.Name = "Levi Ackermen";
+            student.Identitas = "Panglima Perang";
+            student.StatusId();
+
+            
+
+
+        }
+    }
+}
---
+++ Inheritance/person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class person
+    {
+        public string Name { get; set; }
+
+        public string Addres { get; set; }
+        public string Identitas { get; set; }
+
+        public void Ngomong()
+        {
+            Console.WriteLine("Nama saya adalah : {0}", Name);
+        }
+
+        public void TempatId()
+        {
+            Console.WriteLine("Saya adalah {0}, saya berada di  {1}", Name, Addres);
+
+        }
+
+        public void StatusId()
+        {
+            Console.WriteLine("Saya seorang {0} , dan nama saya adalah {1}", Identitas, Name);
+        }
+    }
+}
